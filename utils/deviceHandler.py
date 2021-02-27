5f5e0e1fcfea0e5ec500110e057e9b9573046e69
Match85 <match.match@gmail.com>
2021-02-27 16:41:52 +0100

    Added logging for routines

--- utils/deviceHandler.py
+++ utils/deviceHandler.py
@@ -9,7 +9,7 @@ import requests
 from datetime import date
 today = date.today()
 import logging
-logging.basicConfig(filename='../logs/' + str(today) + '.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+logging.basicConfig(filename='../logs/' + str(today) + '.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
 
 from utils.pyS150 import MotionSensor
 from utils.pyW215 import SmartPlug, ON, OFF
