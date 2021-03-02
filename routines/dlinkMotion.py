c7c13d19521c0393227088d283beca08b53045f3
Match85 <match.match@gmail.com>
2021-03-02 17:15:11 +0100

    added sleep to dlink motion timeout

--- routines/dlinkMotion.py
+++ routines/dlinkMotion.py
@@ -8,12 +8,12 @@ import logging
 last = deviceHandler.getMotionState()
 
 while True:
+	time.sleep(60)
 	now = deviceHandler.getMotionState()
 	if now > last:
 		logging.info("Motion detected on DLink")
 		deviceHandler.setLightState(4, "on")
 		last = now
-		time.sleep(60)
 	if (time.time() > last + 60) and (deviceHandler.getLightState(4)):
 		logging.info("No motion detected on DLink for 1 minute")
 		deviceHandler.setLightState(4, "off")
