3cb711fa11b8a82007484cf5c0fa21a0e270b339
match85 <match.match@gmail.com>
2021-10-30 17:33:10 +0200

    debug

--- routines/comeHome.py
+++ routines/comeHome.py
@@ -11,9 +11,9 @@ import logging
 currentTime = datetime.datetime.now().hour
 lastSeen = statusHandler.getPhoneLast()
 while (not statusHandler.getPhoneState()) and ((currentTime >= 17) or (currentTime <= 5)):
-    logging.info("Coming home investigation started")
+    #logging.info("Coming home investigation started")
     time.sleep(10)
     # deviceHandler.getPhoneState(0)
-    if (statusHandler.getPhoneState() + 1800) < time.time():
+    if (statusHandler.getPhoneLast() + 1800) < time.time():
         logging.info("User came home detected")
         deviceHandler.setLightState(3, 'on')
