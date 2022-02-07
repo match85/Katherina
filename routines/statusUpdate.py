20651e055c137e53fce8ae74c189c195c268a4ee
match85 <match.match@gmail.com>
2022-02-07 18:21:36 +0100

    temporary status update script

---
+++ routines/statusUpdate.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.append("..")
+from utils import deviceHandler
+from utils import statusHandler
+
+statusHandler.setLightState(1, deviceHandler.getLightState(1))
+statusHandler.setLightState(2, deviceHandler.getLightState(2))
+statusHandler.setLightState(3, deviceHandler.getLightState(3))
+statusHandler.setLightState(4, deviceHandler.getLightState(4))
+
+statusHandler.setPlugState(0, deviceHandler.getPlugState(0))
+statusHandler.setPlugState(1, deviceHandler.getPlugState(1))
