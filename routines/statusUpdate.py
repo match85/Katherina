fa63b87ead92edbeaad36ff5ebab853b3c9a7d1f
match85 <match.match@gmail.com>
2022-12-03 14:52:26 +0100

    status updater fix

--- routines/statusUpdate.py
+++ routines/statusUpdate.py
@@ -8,6 +8,7 @@ statusHandler.setLightState(1, deviceHandler.getLightState(1))
 statusHandler.setLightState(2, deviceHandler.getLightState(2))
 statusHandler.setLightState(3, deviceHandler.getLightState(3))
 statusHandler.setLightState(4, deviceHandler.getLightState(4))
+statusHandler.setLightState(4, deviceHandler.getLightState(5))
 
 statusHandler.setPlugState(0, deviceHandler.getPlugState(0))
 statusHandler.setPlugState(1, deviceHandler.getPlugState(1))
