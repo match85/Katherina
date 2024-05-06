83e6da8d063470256e378e7297e39e1589e85885
match85 <match.match@gmail.com>
2024-05-06 06:38:28 +0200

    add exception skip for onmessage in mqtt

--- Test/mqtt.py
+++ Test/mqtt.py
@@ -23,82 +23,84 @@ doorSensor = deviceInfo.getXiaomiData('door', 1, 'zigbeeName')
 switch = deviceInfo.getPhilipsData('switch', 1, 'zigbeeName')
 
 def on_message(client, userdata, message):
-    importlib.reload(routineInfo)
-    response = json.loads(message.payload.decode("utf8"))
-    logging.info("Message received: " + str(message.topic))
-    if message.topic == kitchenMotion:
-        statusHandler.setMotionState(1, response['occupancy'])
-        if response['occupancy']:
-            logging.info("Motion detected in kitchen")
-            statusHandler.setMotionLast(1, time.time())
-            if not statusHandler.getLightState(2):
-                if int(response['illuminance']) < routineInfo.getRoutineData("kitchenMotion", "minIlluminance"):
+    try:
+        importlib.reload(routineInfo)
+        response = json.loads(message.payload.decode("utf8"))
+        logging.info("Message received: " + str(message.topic))
+        if message.topic == kitchenMotion:
+            statusHandler.setMotionState(1, response['occupancy'])
+            if response['occupancy']:
+                logging.info("Motion detected in kitchen")
+                statusHandler.setMotionLast(1, time.time())
+                if not statusHandler.getLightState(2):
+                    if int(response['illuminance']) < routineInfo.getRoutineData("kitchenMotion", "minIlluminance"):
+                        deviceHandler.setLightState(1, True)
+                else:
                     deviceHandler.setLightState(1, True)
-            else:
-                deviceHandler.setLightState(1, True)
 
-    if message.topic == hallwayMotion:
-        statusHandler.setMotionState(2, response['occupancy'])
-        print(routineInfo.getRoutineData("kitchenMotion", "minIlluminance"))
-        if response['occupancy']:
-            logging.info("Motion detected in hallway")
-            statusHandler.setMotionLast(2, time.time())
-            if not statusHandler.getLightState(1):
-                if int(response['illuminance']) < routineInfo.getRoutineData("kitchenMotion", "minIlluminance"):
+        if message.topic == hallwayMotion:
+            statusHandler.setMotionState(2, response['occupancy'])
+            print(routineInfo.getRoutineData("kitchenMotion", "minIlluminance"))
+            if response['occupancy']:
+                logging.info("Motion detected in hallway")
+                statusHandler.setMotionLast(2, time.time())
+                if not statusHandler.getLightState(1):
+                    if int(response['illuminance']) < routineInfo.getRoutineData("kitchenMotion", "minIlluminance"):
+                        deviceHandler.setLightState(2, True)
+                else:
                     deviceHandler.setLightState(2, True)
-            else:
-                deviceHandler.setLightState(2, True)
 
-            if deviceHandler.goneHome():
-                deviceHandler.setLightState(3, True)
+                if deviceHandler.goneHome():
+                    deviceHandler.setLightState(3, True)
+                try:
+                    requests.get(deviceHandler.getTabletUrl() + "motion=true")
+                except:
+                    pass
+
+        if message.topic == 'zigbee2mqtt/bath_motion':
+            statusHandler.setMotionState(3, response['occupancy'])
+            if response['occupancy']:
+                logging.info("Motion detected in bathroom")
+                statusHandler.setMotionLast(3, time.time())
+                deviceHandler.setLightState(4, True)
+
+        if message.topic == doorSensor:
+            statusHandler.setDoorState(1, response['contact'])
+            logging.info("Door sensor " + str(response['contact']))
+
+        if message.topic == switch:
             try:
-                requests.get(deviceHandler.getTabletUrl() + "motion=true")
+                if response['action'] == 'on-press':
+                    if not statusHandler.getLightState(3):
+                        deviceHandler.setLightState(3, True)
+                    else:
+                        deviceHandler.setLightState(3, False)
+
+                if response['action'] == 'off-press':
+                    print('off button')
+
+                if response['action'] == 'up-press':
+                    min_temp = float(routineInfo.getRoutineData("temp", "min_temp"))
+                    max_temp = float(routineInfo.getRoutineData("temp", "max_temp"))
+                    routineInfo.setRoutineData("temp", "min_temp", str(min_temp + 0.5))
+                    routineInfo.setRoutineData("temp", "max_temp", str(max_temp + 0.5))
+                    #text = "Temperature increased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp")
+                    logging.info("Temperature increased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp"))
+                    #deviceHandler.alexaSay(text)
+
+                if response['action'] == 'down-press':
+                    min_temp = float(routineInfo.getRoutineData("temp", "min_temp"))
+                    max_temp = float(routineInfo.getRoutineData("temp", "max_temp"))
+                    routineInfo.setRoutineData("temp", "min_temp", str(min_temp - 0.5))
+                    routineInfo.setRoutineData("temp", "max_temp", str(max_temp - 0.5))
+                    #text = "Temperature decreased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp")
+                    logging.info("Temperature decreased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp"))
+                    #deviceHandler.alexaSay(text)
+
             except:
                 pass
-
-    if message.topic == 'zigbee2mqtt/bath_motion':
-        statusHandler.setMotionState(3, response['occupancy'])
-        if response['occupancy']:
-            logging.info("Motion detected in bathroom")
-            statusHandler.setMotionLast(3, time.time())
-            deviceHandler.setLightState(4, True)
-
-    if message.topic == doorSensor:
-        statusHandler.setDoorState(1, response['contact'])
-        logging.info("Door sensor " + str(response['contact']))
-
-    if message.topic == switch:
-        try:
-            if response['action'] == 'on-press':
-                if not statusHandler.getLightState(3):
-                    deviceHandler.setLightState(3, True)
-                else:
-                    deviceHandler.setLightState(3, False)
-
-            if response['action'] == 'off-press':
-                print('off button')
-
-            if response['action'] == 'up-press':
-                min_temp = float(routineInfo.getRoutineData("temp", "min_temp"))
-                max_temp = float(routineInfo.getRoutineData("temp", "max_temp"))
-                routineInfo.setRoutineData("temp", "min_temp", str(min_temp + 0.5))
-                routineInfo.setRoutineData("temp", "max_temp", str(max_temp + 0.5))
-                #text = "Temperature increased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp")
-                logging.info("Temperature increased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp"))
-                #deviceHandler.alexaSay(text)
-
-            if response['action'] == 'down-press':
-                min_temp = float(routineInfo.getRoutineData("temp", "min_temp"))
-                max_temp = float(routineInfo.getRoutineData("temp", "max_temp"))
-                routineInfo.setRoutineData("temp", "min_temp", str(min_temp - 0.5))
-                routineInfo.setRoutineData("temp", "max_temp", str(max_temp - 0.5))
-                #text = "Temperature decreased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp")
-                logging.info("Temperature decreased to " + routineInfo.getRoutineData("temp", "min_temp") + "-" + routineInfo.getRoutineData("temp", "max_temp"))
-                #deviceHandler.alexaSay(text)
-
-        except:
-            pass
-
+    except:
+        pass
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
